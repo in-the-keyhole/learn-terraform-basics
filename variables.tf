a6d4820cbb849befdfaeed149beb571602d1bd99
Jaime Niswonger <jniswonger@keyholesoftware.com>
2021-08-26 14:26:42 +0000

    added default.auto.tfvars

--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-2"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
 }
