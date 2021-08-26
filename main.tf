fe6ce2679d5e57d15f0989575c6308c5e4fc3cfb
Jaime Niswonger <jniswonger@keyholesoftware.com>
2021-08-26 17:51:21 +0000

    cleanup

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ provider "aws" {
   region = var.region
 }
 
+
 data "aws_ami" "ubuntu" {
   most_recent = true
-
+  owners      = ["099720109477"]
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
@@ -14,8 +15,6 @@ data "aws_ami" "ubuntu" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "ubuntu" {
--- versions.tf
+++ versions.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.28.0"
     }
-
-    # random = {
-    #   source  = "hashicorp/random"
-    #   version = "3.0.0"
-    # }
   }
 
   required_version = ">= 0.14.0"
